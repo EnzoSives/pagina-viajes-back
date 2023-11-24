1c49330a22bbce959188d64ac665f51fe5f4bc24
EnzoSives <Enzosives7@gmail.com>
2023-11-24 12:23:24 -0300

    Auth funciona

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,33 +1,29 @@
-import { AuthService } from './auth.service';
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { LoginDto } from './dto/login.dto';
-import { RegisterDto } from './dto/register.dto';
-import { AuthGuard } from './auth.guard';
-
+import { Controller, Post, Body, Get, Request, UseGuards } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { LoginDto } from "./dto/login.dto";
+import { RegisterDto } from "./dto/register.dto";
+import { AuthGuard } from "./auth.guard";
 
 @Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+export class AuthController{
+    constructor ( private readonly authService: AuthService){}
 
-  @Post('register')
-  register(@Body() registerDto: RegisterDto) {
-    // console.log(registerDto);
-    return this.authService.register(registerDto);
-  }
+    @Post('register')
+    register(@Body() registerDto: RegisterDto){
+        return this.authService.register(registerDto);
+    }
 
-  @Post('login')
-  login(@Body() loginDto: LoginDto) {
-    // console.log(loginDto);
-    return this.authService.login(loginDto);
-  }
+    @Post('login')
+    login(@Body() loginDto: LoginDto){
+        return this.authService.login(loginDto)
+    }
 
- 
+    @UseGuards(AuthGuard)
     @Get('user')
     async getUser(@Request() req) {
         // req.user contendrá la información del usuario autenticado
-        const { email } = req.body;
+        const { email } = req.user;
         const user = await this.authService.findUserByEmail(email);
         return { user };
-        
     }
 }
